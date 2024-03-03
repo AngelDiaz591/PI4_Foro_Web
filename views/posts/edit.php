d7e1d280e077cb9696c168b46be2da30be10c01a
Zama15 <alexavdstiles@gmail.com>
2024-03-02 18:33:38 -0600

    Modify views
    Add components to handle the images case

--- views/posts/edit.php
+++ views/posts/edit.php
@@ -1,6 +1,6 @@
 <div>
   <h1>Edit Post</h1>
-  <form action="<?= redirect_to('posts', 'patch'); ?>" method="post">
+  <form action="<?= redirect_to('posts', 'patch'); ?>" method="post" enctype="multipart/form-data">
     <input type="hidden" name="id" value="<?= $params["id"]; ?>">
     <div>
       <label for="title">Title</label>
@@ -9,6 +9,20 @@
     <div>
       <label for="description">Description</label>
       <input type="text" name="description" id="description" placeholder="Description" value="<?= $params["description"]; ?>" required>
+    </div>
+    <div class="images">
+      <?php foreach ($params["images"] as $image): ?>
+        <div class="image">
+          <img src="<?= get_home_url() . "assets/imgs/" . $image["image"] ?>" alt='Image from "<?= $params["title"] ?>"'>
+          <a href="<?= redirect_to('posts', 'purge_image') . '&id=' . $image["id"] . '&image=' . $image["image"] . '&post_id=' . $params["id"]; ?>">
+            <span>&times;</span>
+          </a>
+        </div>
+      <?php endforeach; ?>
+    </div>
+    <div>
+      <label for="images">Images</label>
+      <input type="file" name="images[]" id="images" accept="image/jpg, image/jpeg, image/png, image/gif" multiple>
     </div>
     <div>
       <button type="submit">Edit</button>
--- views/posts/show.php
+++ views/posts/show.php
@@ -3,6 +3,11 @@
   <div>
     <h2><?= $params["title"] ?></h2>
     <p><?= $params["description"] ?></p>
+    <div class="images">
+      <?php foreach ($params["images"] as $image): ?>
+        <img src="<?= get_home_url() . "assets/imgs/" . $image["image"] ?>" alt='Image from "<?= $params["title"] ?>"'>
+      <?php endforeach; ?>
+    </div>
   </div>
   <a href="<?= redirect_to('posts', 'index'); ?>">Back</a>
 </div>
