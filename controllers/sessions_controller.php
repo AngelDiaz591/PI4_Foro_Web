9ca0126cd46b6c3812e214c9f26295ae2e24396b
Zama15 <alexavdstiles@gmail.com>
2024-03-17 09:45:29 -0600

    modify Sessions code
    Refactorized controller, move sessions model to users, modified views

--- controllers/sessions_controller.php
+++ controllers/sessions_controller.php
@@ -1,38 +1,49 @@
 <?php
 ob_start();
-get_model('session');
+get_model('user');
 
-class SessionsController extends Session  {
-    private $params;
-    
-    public function __construct($params) {
-        // var_dump($params);
-        try {
-            parent::__construct();
-            $this->params = $params['method'];
-        } catch (Exception $e) {
-            error_log($e->getMessage());
-            redirect_to_error('500');
-        }
+class SessionsController extends User  {
+  private $params;
+  
+  public function __construct($params) {
+    try {
+      parent::__construct();
+      $this->params = $params['method'];
+    } catch (Exception $e) {
+      $this->handle_exception_redirect_error($e, '500');
     }
+  }
 
-    public function create() {
-        try {
-            $user = $this->login($this->params);
-            if ($user !== null) {
-                $email = $user['email']; // Get the user's email from the data returned by the login function
-                // Redirect the user to a success page or wherever necessary
-                header("Location:" . redirect_to('posts', 'index'));
-                exit();
-            } else {
-                throw new Exception("Failed login");
-            }
-        } catch (Exception $e) {
-            session_start();
-            $_SESSION['error'] = $e->getMessage();
-            header("Location: ../login/login.php?error=");
-            exit();
-        }
+  public function new() {
+    return $this->render('new', $this->params);
+  }
+
+  public function create() {
+    try {
+      $response = $this->verify_credentials($this->params);
+      if ($response["status"]) {
+        $_SESSION["user"] = $response["data"];
+        header("Location:" . redirect_to('posts', 'index'));
+      } else {
+        throw new Exception("Failed to login user: " . $response["message"]);
+      }
+    } catch (Exception $e) {
+      $_SESSION['error'] = $e->getMessage();
+      $this->handle_exception_redirect_to($e, 'sessions', 'new');
     }
+  }
+
+  public function destroy() {
+    session_destroy();
+    header("Location:" . redirect_to('posts', 'index'));
+  }
+
+  protected function render($view, $data = []) {
+    $params = $data;
+
+    include ROOT_DIR . 'views/users/sessions/' . $view . '.php';
+
+    return ob_get_clean();
+  }
 }
 ?>
