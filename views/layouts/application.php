f8eaa16d6e74c2c6a601c9aeb515f7bbd475acbc
Zama15 <alexavdstiles@gmail.com>
2024-03-17 09:35:17 -0600

    Resolve conflictis between some styles, add more resources

--- views/layouts/application.php
+++ views/layouts/application.php
@@ -1,6 +1,8 @@
 <?php 
 include './../../controllers/application_controller.php';
 
+session_start();
+
 if (empty($_GET['controller']) || empty($_GET['action'])) {
   redirect_to_error('404');
 }
@@ -12,35 +14,69 @@ $data = array(
   "method" => $_POST ? $_POST : $_GET,
   "files" => $_FILES,
 );
+
+if ($action === 'create' || $action === 'update' || $action === 'destroy') {
+  if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    redirect_to_error('405');
+  }
+
+  render($action, $controller, $data);
+  exit;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
+  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Visitante</title>
-  
+  <title>CulturEdge</title>
+  <!-- Favicon -->
+  <?= link_tag('icon', 'img/favicon.ico', 'image/x-icon'); ?>
+  <!-- FONTS -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@700&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
-  <?= link_tag('stylesheet', 'main'); ?>
-
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Almarai&family=Inter&family=Lato&family=Roboto+Slab&family=Rubik&family=Poppins&display=swap">
+  <!-- ICONS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link rel='stylesheet' href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css'>
+  <!-- STYLESHEETS -->
+  <?= link_tag('stylesheet', 'stylesheets/main.css'); ?>
+  <!-- SCRIPT -->
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <?= script_tag('main'); ?>
 </head>
 <body>
-  <?php render_layout('header'); ?>
-  
-  <div class="container">
-    <?php render_layout('sidebar_main'); ?>
-
-    <main>
+  <?php if ($controller === 'sessions' && $action === 'new' || 
+              $controller === 'registrations' && $action === 'new' ||
+              $controller === 'passwords' && $action === 'new' ||
+              $controller === 'passwords' && $action === 'edit' ||
+              $controller === 'confirmations' && $action === 'new'): ?>
+    <div class="user-view-wrapper">
       <?= render($action, $controller, $data); ?>
-    </main>
+    </div>
+  <?php else: ?>
+    <?php render_layout('header'); ?>
+    
+    <div class="container">
+      <nav id="main-nav">
+        <?php render_layout('sidebar_main'); ?>
+      </nav>
 
-    <?php render_layout('sidebar_chats'); ?>
-  </div>
+      <main>
+        <?= render($action, $controller, $data); ?>
+      </main>
+      
+      <div id="modal" class="modal-container">
+        <div class="modal-header">
+          <i class='bx bx-error'></i>
+          <h2>log in to interact</h2>
+        </div>
+        <span class="close-modal" onclick="closeModal()">&times;</span>
+      </div>
+
+      <?php render_layout('sidebar_chats'); ?>
+    </div>
+  <?php endif; ?>
 </body>
 </html>
