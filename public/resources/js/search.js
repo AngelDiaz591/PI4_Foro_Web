eb39dc9c9b72a78f95d2e3e463c3b3769b4b0d4e
Gavalencia12 <gavalencia0422@gmail.com>
2024-05-22 22:46:57 -0600

    corrected search bar

--- public/resources/js/search.js
+++ public/resources/js/search.js
@@ -11,7 +11,7 @@ document.getElementById('search-input').addEventListener('input', debounce(funct
     const resultsContainer = document.getElementById('search-results');
 
     if (query.length === 0) {
-        resultsContainer.innerHTML = ''; 
+        resultsContainer.innerHTML = '';
         return;
     }
 
@@ -26,50 +26,50 @@ document.getElementById('search-input').addEventListener('input', debounce(funct
     .then(data => {
         resultsContainer.innerHTML = '';
         if (data.error) {
-            resultsContainer.innerHTML = `<div class="not-found"><p>${data.error}</p></div>`;
+            resultsContainer.innerHTML = `<div class="not-found box-result"><p>${data.error}</p></div>`;
         } else if (data.length === 0) {
-            resultsContainer.innerHTML = '<div class="not-found"><p>No found result.</p></div>';
+            resultsContainer.innerHTML = '<div class="not-found box-result"><p>No found result.</p></div>';
         } else {
             data.forEach(d => {
                 if (d.title.toLowerCase().includes(query) || 
                     d.description.toLowerCase().includes(query) || 
-                    d.theme.toLowerCase().includes(query)) { 
+                    d.theme.toLowerCase().includes(query) || 
+                    d.username.toLowerCase().includes(query)) {  
                     const postHtml = `
-                            <div class="box-result" id="results-list">
-                                <a href="/users/show/id:${d.user_id}">
-                                    <div class="user_card">
-                                        <img src="/resources/img/user.png" alt="user" class="user-card-img">
-                                        <p class="profile-card"><p>${d.username}</p></p>
-                                    </div>
-                                </a>
-                                <div class="vi">
-                                    <a href="/posts/show/id:${d.id}">
-                                        <div class="" id="results-list-${d.id}">
-                                            <p class="text-theme"><i class="${d.theme_icon}"></i> ${d.theme}</p>
-                                            <div class="text">
-                                                <h2>${d.title}</h2>
-                                                <div class="actions">
-                                                    <div class="info">
-                                                        <a href="/posts/show/id:${d.id}">
-                                                            <center>
-                                                                <p id="imagenDinamica"><i class='bx bx-show-alt'></i> Vision</p>
-                                                            </center>
-                                                        </a>
-                                                    </div>
-                                                </div>
+                    <div class="box-result" id="results-list">
+                        <a href="/users/show/id:${d.user_id}">
+                            <div class="user_card">
+                                <img src="/resources/img/user.png" alt="user" class="user-card-img">
+                                <p class="profile-card"><p>${d.username}</p></p>
+                            </div>
+                        </a>
+                        <div class="vi">
+                            <a href="/posts/show/id:${d.id}">
+                                <div class="" id="results-list-${d.id}">
+                                    <p class="text-theme"><i class="${d.theme_icon}"></i> ${d.theme}</p>
+                                    <div class="text">
+                                        <h2>${d.title}</h2>
+                                        <div class="actions">
+                                            <div class="info">
+                                                <a href="/posts/show/id:${d.id}">
+                                                    <center>
+                                                        <p id="imagenDinamica"><i class='bx bx-show-alt'></i> Vision</p>
+                                                    </center>
+                                                </a>
                                             </div>
                                         </div>
-                                    </a>
+                                    </div>
                                 </div>
-                            </div>
+                            </a>
+                        </div>
+                    </div>
                     `;
                     resultsContainer.innerHTML += postHtml;
                 }
             });
-            
         }
     })
     .catch(error => {
         console.error('Error fetching search results:', error);
     });
-}, 300)); 
+}, 300));
