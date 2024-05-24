3143e13264f6478f755227c7c106c5e18527e87e
ismael <imontiel@ucol.mx>
2024-05-23 22:53:38 -0600

    user controll ready

--- public/resources/js/useradmin.js
+++ public/resources/js/useradmin.js
@@ -1,5 +1,5 @@
 function showDeleteConfirmation(userId) {
     if (confirm("Are you sure you want to delete this user?")) {
-        window.location.href = "/admins/user_delete?id=" + userId;
+        window.location.href = "/admins/user_delete/?id=" + userId;
     }
 }
