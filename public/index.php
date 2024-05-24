29c8d7a0773a10fe02260f49d2041bb6275c3190
ismael <imontiel@ucol.mx>
2024-05-24 08:11:59 -0600

    english ready

--- public/index.php
+++ public/index.php
@@ -19,7 +19,7 @@ $role_layouts = [
 $default_layout = 'sidebar_main';
 ?>
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
