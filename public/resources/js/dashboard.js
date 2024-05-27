c6aedf94863501fdf596fe736f878c11486aaf0b
MeganPegueros <mpegueros@ucol.mx>
2024-05-27 01:12:38 -0600

    dashboard

--- public/resources/js/dashboard.js
+++ public/resources/js/dashboard.js
@@ -1,38 +1,55 @@
-
 var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['End of Poverty', 'Zero Hunger', 'Health and Wellness', 'Quality Education'],
-        datasets: [{
-            label: 'Total Number of Publications',
-            data: [80, 65, 75, 85], // Valores de ejemplo
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-      plugins: {
-          title: {
-              display: true,
-              text: 'Most Popular Topics'
-          }
-      },
-      scales: {
-          y: {
-              beginAtZero: true
-          }
-      }
-    }
-});
+        var myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['End of Poverty', 'Zero Hunger', 'Health and Wellness', 'Quality Education'],
+                datasets: [{
+                    label: 'Total Number of Publications',
+                    data: [80, 65, 75, 85], // Valores de ejemplo
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255, 99, 132, 1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true, // Hacer la gráfica responsiva
+                /* maintainAspectRatio: false, // Permitir que la gráfica se ajuste a cualquier proporción */
+                plugins: {
+                    title: {
+                        display: true,
+                        text: 'Temas Más Populares', // Título de la gráfica
+                        font: {
+                            size: 24, // Tamaño de la fuente
+                            family: 'Arial, sans-serif', // Familia de la fuente
+                            weight: 'bold', // Peso de la fuente
+                            color: '#333' // Color de la fuente
+                        },
+                        padding: {
+                            top: 10,
+                            bottom: 30
+                        },
+                        align: 'start'
+                    }
+                },
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+
+        // Agregar un evento de redimensionamiento para forzar la actualización de la gráfica
+        window.addEventListener('resize', function() {
+            myChart.resize();
+        });
--- views/admins/console.php
+++ views/admins/console.php
@@ -1,32 +1,38 @@
-<div class="chart-container">
-  <canvas id="myChart"></canvas>
-</div>
-
-<div class="general-data">
-  <div class="boxed-data">
-    <i class='bx bxs-collection'></i>
-    <p>Topics: 15</p>
-  </div>
-  <div class="boxed-data">
-    <i class='bx bxs-notepad'></i>
-    <p>Total Publications: 305</p>
-  </div>
-  <div class="boxed-data">
-    <i class='bx bxs-chat'></i>
-    <p>Comments: 2000</p>
-  </div>
-  <div class="boxed-data">
-    <i class='bx bxs-like'></i>
-    <p>Likes: 1355</p>
-</div>
-  <div class="boxed-data">
-    <i class='bx bxs-user-circle'></i>
-    <p>Active Users: 19</p>
-  </div>
-  <div class="boxed-data">
-    <i class='bx bxs-group'></i>
-    <p>Total Users: 135</p>
-  </div>
-</div>
-
+<div class="body_wrapper">
+    <div class="welcome-admin">
+      <h1>WELCOME BACK, <?= $data->username ?>!</h1>
+      <p>Good to see you again!</p>
+      <h6>Overview</h6>
+    </div>
+    <div class="general-data">
+      <div class="boxed-data">
+        <img src="/resources/img/icon-user.svg" alt="">
+        <div>
+          <p class="count5">135</p>
+          <p class="statistics">Total Users</p>
+        </div>
+      </div>
+      <div class="boxed-data">
+          <img src="/resources/img/icon-reaction.svg" alt="">
+        <div>
+          <p class="count4">1355</p>
+          <p class="statistics">Total Reactions</p>
+        </div>
+    </div>
+    <div class="boxed-data">
+        <img src="/resources/img/icon-post.svg" alt="">
+        <div>
+          <p class="count2">305</p>
+          <p class="statistics">Total Posts</p>
+        </div>
+      </div>
+    </div>
+    <div class="chart-container">
+      <div class="graphics">
+        <canvas id="myChart">
+        </canvas>
+      </div>
+    </div>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="/resources/js/dashboard.js"></script>
